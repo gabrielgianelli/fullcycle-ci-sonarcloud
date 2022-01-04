36442e5f869315040448e9051c7d081cb467c3db
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-01-04 19:23:13 -0300

    test: :white_check_mark: add test to diff function

--- go/sum_test.go
+++ go/sum_test.go
@@ -10,3 +10,12 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
+
+func TestDiff(t *testing.T) {
+
+	result := diff(3, 2)
+
+	if result != 1 {
+		t.Error("The result must be 1")
+	}
+}
