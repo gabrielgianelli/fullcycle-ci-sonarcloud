32a821e22de2b89d0385b7e1e392d38fbf648012
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-01-04 19:29:50 -0300

    refactor(test): :recycle: improve tests segregation

--- go/sum_test.go
+++ go/sum_test.go
@@ -10,12 +10,3 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
-
-func TestDiff(t *testing.T) {
-
-	result := diff(3, 2)
-
-	if result != 1 {
-		t.Error("The result must be 1")
-	}
-}
